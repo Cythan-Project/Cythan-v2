d777436c4620ed71b453b6df608808877460272e
Cypooos <60020572+Cypooos@users.noreply.github.com>
2020-08-25 19:31:05 +0200

    Public struct & more comments for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 /*!
- * The Cythan machine emulator librairie.
+ * The Cythan machine emulator librairy.
  * 
  * The Cythan machine is a mathematical Turing Complete computer.
  * The machine is composed of one vector. Each value of the vector is a positive integer, "pointing" to another value.
@@ -8,9 +8,22 @@
  * 
  *  - The first case (the pointer), is incremented by 2.
  * 
- *  - The 2 cases pointed by the first case before the incrementation is "executed", which mean that the case pointed by the first executed case will be copied over the case pointed by the second one.
+ *  - The 2 cases pointed by the first case before the incrementation is "executed". In a pair of executed cases, the case that as for index the second value is set to the value of the case that have as index the first value
  * 
- * For exemple, `1,5,3,0,0,999` will copied the content of the 5th case (999) into the 3rd one. The result after one iteration will be `3,5,3,999,0,999`
+ * For instance, `1,5,3,0,0,999` will copy the content of the 5th case (999) into the 3rd one. The result after one iteration will be `3,5,3,999,0,999`
+ * 
+ * ### Example
+ * 
+ * ```rust
+ * use cythan::Cythan;
+ * let mut cythan = Cythan::new( vec![1,9,5,10,1,0,0,11,0,1,20,21] );
+ * println!("Cythan start:{:?}",cythan);
+ * for a in 0..10 {
+ *    cythan.next();
+ *    println!("Cythan iteration {}:{:?}",a,cythan)
+ * }
+ * ```
+ * 
 */
 
 
@@ -27,7 +40,7 @@ use test::Bencher;
 
 /// The main structure of the Cythan machine
 #[derive(Debug)]
-struct Cythan {
+pub struct Cythan {
     cases: Vec<usize>
 }
 
@@ -37,6 +50,7 @@ impl Cythan{
     /// `cases` is a vector that represent the base code of the Cythan machine
     /// ### Example
     /// ```rust
+    /// use cythan::Cythan;
     /// let mut cythan = Cythan::new( vec![1,9,5,10,1,0,0,11,0,1,20,21] );
     /// println!("Cythan start:{:?}",cythan);
     /// for a in 0..10 {
